310d6b55a7d9d5b5b437b7d6b5a884efe6eccfd3
Elen-Korneeva <elenakor87@icloud.com>
2023-11-20 19:14:59 +0300

    add Task06.java

---
+++ src/Task06.java
@@ -0,0 +1,16 @@
+public class Task06 {
+
+    static int COUNT = 0;
+
+    public static void main(String[] args) {
+        System.out.println("Задание №6");
+        System.out.println(sum(10));
+
+    }
+        static  int sum(int number) {
+        int sum = number + 10;
+        return  sum;
+        }
+
+    }
+
